0a7898fb2cdf50dfd86eeb07d249791d6630ceb6
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-01-24 18:23:35 -0600

    autosave script

--- src/main/resources/static/js/projects.js
+++ src/main/resources/static/js/projects.js
@@ -1,47 +1,38 @@
-let path = document.querySelector('path');
-let pathLength = path.getTotalLength();
-
-path.style.strokeDasharray = pathLength + ' ' + pathLength;
-
-path.style.strokeDashoffset = pathLength;
-
-window.addEventListener('scroll', function(e) {
-    let scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-    let drawLength = pathLength * scrollPercentage;
-
-    path.style.strokeDashoffset = pathLength - drawLength;
-
-    const target = document.querySelector('.scroll');
-
-    let index = 0, length = target.length;
-    for (index; index < length; index++) {
-        let pos = window.pageYOffset * target[index].dataset.rate;
-
-        if (target[index].dataset.direction === 'vertical') {
-            target[index].style.transform = 'translate3d(0px,' + pos + 'px, 0px)';
-        }else{
-            let posX = window.pageYOffset * target[index].dataset.ratex;
-            let posY = window.pageYOffset * target[index].dataset.ratey;
-
-            target[index].style.transform = 'translate3d(' + posX + 'px,' + posY + 'px, 0px)';
-        }
-    }
-});
+// let path = document.querySelector('path');
+// let pathLength = path.getTotalLength();
+//
+// path.style.strokeDasharray = pathLength + ' ' + pathLength;
+//
+// path.style.strokeDashoffset = pathLength;
+//
+// window.addEventListener('scroll', function(e) {
+//     let scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+//     let drawLength = pathLength * scrollPercentage;
+//
+//     path.style.strokeDashoffset = pathLength - drawLength;
+//
+//     const target = document.querySelector('.scroll');
+//
+//     let index = 0, length = target.length;
+//     for (index; index < length; index++) {
+//         let pos = window.pageYOffset * target[index].dataset.rate;
+//
+//         if (target[index].dataset.direction === 'vertical') {
+//             target[index].style.transform = 'translate3d(0px,' + pos + 'px, 0px)';
+//         }else{
+//             let posX = window.pageYOffset * target[index].dataset.ratex;
+//             let posY = window.pageYOffset * target[index].dataset.ratey;
+//
+//             target[index].style.transform = 'translate3d(' + posX + 'px,' + posY + 'px, 0px)';
+//         }
+//     }
+// });
 
 //Show Dialog
-$(document).ready(function() {
-    $('.clickForMoreInfo').on("click", function (e) {
-        console.log("clicked");
-        // e.preventDefault();
-        // $(".dialog").show();
-        // $(this).parents(".img_overlay_bottom").siblings(".dialog").show();
-    });
-
-    $('.close-button').click(function (e) {
-        e.preventDefault();
-        $(this).parents('.dialog').hide();
-    });
+$(".clickForMoreInfo").click(function () {
+    $(this).next(".dialog").show();
 });
+
 
 
 //close dialog on click outside
