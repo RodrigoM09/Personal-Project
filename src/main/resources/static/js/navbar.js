13e23d0876fca8ec4cdd397adfcd5b88e79af7cb
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-03-02 17:01:00 -0600

    autosave script

--- src/main/resources/static/js/navbar.js
+++ src/main/resources/static/js/navbar.js
@@ -24,8 +24,3 @@ modeSwitch.addEventListener("click" , () =>{
 
     }
 });
-
-window.addEventListener('load', () => {
-    const doc = document.documentElement
-    doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
-});
